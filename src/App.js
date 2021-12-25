4e29262751d2ca64e374dfc083ae0cfa4580a07e
patrickRobotics <pirungu9@gmail.com>
2021-12-25 13:25:14 +0300

    abstract get server data into a resusable function

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import { DataSource } from "./containers/DataSource";
 import { UserInfo } from "./components/people/UserInfo";
 import { ProductInfo } from "./components/products/ProductInfo";
 
+const getServerData = url => async() => {
+	const response = await axios.get(url);
+	return response.data;
+}
+
 function App() {
 	return (
 		<>
-		< DataSource getDataFunc={async() => {
-			const response = await axios.get(`/users/123`);
-            return response.data;
-		}} resourceName="user">
+		< DataSource getDataFunc={ getServerData('/users/123') } resourceName="user">
 			<UserInfo />
 		</DataSource>
 		</>
