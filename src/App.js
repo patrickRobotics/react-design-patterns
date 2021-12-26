23515cb6122238c4996dc2f6028f061103df0847
patrickRobotics <pirungu9@gmail.com>
2021-12-26 21:27:38 +0300

    uncotrolled onboarding - parent have no control of children's behavior

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
+import { UncontrolledOnboardingFlow } from "./components/UncontrolledOnboardingFlow";
 
-import { useState } from 'react';
+const StepOne = ({ goToNext }) => {
+	return(
+		<>
+		<h1>Step 1</h1>
+		<button onClick={() => goToNext({ name: 'John Doe' })}>Next</button>
+		</>
+	);
+}
+const StepTwo = ({ goToNext }) => {
+	return(
+		<>
+		<h1>Step 2</h1>
+		<button onClick={() => goToNext({ age: 19 })}>Next</button>
+		</>
+	);
+}
+const StepThree = ({ goToNext }) => {
+	return(
+		<>
+		<h1>Step 3</h1>
+		<button onClick={() => goToNext({ hairColor: 'Brown' })}>Next</button>
+		</>
+	);
+}
 
-import { ControlledModal } from "./components/ControlledModal";
 
 function App() {
-	const [shouldShowModal, setShouldShowModal] = useState(false);
 	return (
-		<>
-		<ControlledModal
-			shouldShow={shouldShowModal}
-			onRequestClose={() => setShouldShowModal(false)}>
-				<h1>Hello world!</h1>
-		</ControlledModal>
-		<button 
-			onClick={
-				() => setShouldShowModal(!shouldShowModal)
-			}>
-			Show Modal
-		</button>
-		</>
+		<UncontrolledOnboardingFlow onFinish={data => {
+			console.log(data);
+		 	alert('Onboarding completed!');
+		}}>
+			<StepOne />
+			<StepTwo />
+			<StepThree />
+		</UncontrolledOnboardingFlow>
 	);
 }
 
