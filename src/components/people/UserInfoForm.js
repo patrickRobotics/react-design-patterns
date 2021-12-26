4411e6b0733ed4daf358b67fc2569637a94bf9d1
patrickRobotics <pirungu9@gmail.com>
2021-12-26 23:42:37 +0300

    further improvement on our HOC by creating a reusable HOC resource

--- src/components/people/UserInfoForm.js
+++ src/components/people/UserInfoForm.js
@@ -1,6 +1,6 @@
-import { withEditableUser } from "./withEditableUser";
+import { withEditableResource } from '../../containers/withEditableResource';
 
-export const UserInfoForm = withEditableUser(({ user, onChangeUser, onSaveUser, onResetUser }) => {
+export const UserInfoForm = withEditableResource(({ user, onChangeUser, onSaveUser, onResetUser }) => {
     const {name, age, hairColor} = user || {};
     return user ? (
         <>
@@ -20,4 +20,4 @@ export const UserInfoForm = withEditableUser(({ user, onChangeUser, onSaveUser, 
         <button onClick={onSaveUser}>Save</button>
         </>
     ) :  <p>Loading ...</p>;
-}, '123');
+}, '/users/123', 'user');
